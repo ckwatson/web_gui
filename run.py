5c101026358cd1559a0016fc92f9407ff55288c8
Mingyang Li <tslmy@outlook.com>
2025-05-17 13:18:33 -0700

    clean up `run.py`

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from web.main import *
+from web.main import app
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=int("80"), debug=True, threaded=True)
+    app.run(host="127.0.0.1", port=80, debug=True, threaded=True)
